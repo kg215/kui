608d9c3975e2e1b44877b95134efe59fa860243f
karsure <591524605.com>
2019-09-29 23:45:35 +0800

    新增加tab组件

--- lib/tab/Tab.tsx
+++ lib/tab/Tab.tsx
@@ -1,8 +1,90 @@
-import React,{FC} from 'react';
+import React, {FC, Fragment, useCallback, useEffect, useMemo, useState} from 'react';
+import classNames from "classnames";
+import set = Reflect.set;
+import {func} from "prop-types";
+interface TabWrapperProps {
+    checkedKey?: string;
+    onChange?: (checkedKey:TabWrapperProps["checkedKey"])=>void;
+    onTabClick?: (checkedKey:TabWrapperProps["checkedKey"])=>void;
+}
 
-export const Tab:FC=function () {
+function isUndefined(key:any): key is undefined{
+    return key===undefined;
+}
 
-    return <div>
-        Tab
+const TabWrapper:FC<TabWrapperProps> = function (
+    {
+        checkedKey,
+        onChange=()=>{},
+        onTabClick=()=>{},
+        children
+    }
+) {
+    const [key,setKey] = useState<string>(checkedKey);
+    let handleClick = useCallback((key)=>{
+        onTabClick(key);
+        setKey(key);
+    },[]);
+    useEffect(function () {
+        setKey(checkedKey);
+        onChange(checkedKey);
+    },[checkedKey]);
+    useEffect(function(){
+        onChange(key);
+    },[key]);
+
+    return <div className={"ks-tab_wrapper"}>
+        <div className={"ks-tab_nav"}>
+            <div className={"ks-tab_list"}>
+                {
+                    React.Children.map(children,(child:React.ReactElement,index) => {
+                        let id:string = String(isUndefined(child.key)?index:child.key);
+                        return typeof child==="string"?
+                            child
+                            :
+                            <div
+                                onClick={child.props.disabled?void 0:()=>handleClick(id)}
+                                className={classNames("ks-tab_item",
+                                    {
+                                        "ks-tab_checked":!child.props.disabled&&key===id,
+                                        "disabled":child.props.disabled,
+                                    }
+                            )}>
+                                {child.props.tab}
+                            </div>
+
+                    })
+                }
+            </div>
+        </div>
+        <div className={"ks-tab_body"}>
+            {
+                React.Children.map(children,(child:React.ReactElement,index) => {
+                    let id:string = String(isUndefined(child.key)?index:child.key);
+                    return typeof child==="string"?
+                        child
+                        :
+                        <div className={classNames("ks-tab_pane",{
+                        "ks-tab_pane_checked":key===id
+                    })}>
+                        {child.props.children}
+                    </div>
+                })
+            }
+        </div>
     </div>
 };
+TabWrapper.displayName = "Tab";
+interface TabPaneProps {
+    key:string,
+    disabled?:boolean;
+    tab?:string|React.ReactNode,
+}
+export const TabPane:FC<TabPaneProps>=()=><Fragment />
+
+const Tab = Object.assign(TabWrapper,{
+    Pane:TabPane
+});
+
+export {Tab};
+
--- src/view/tabs/Tabs.tsx
+++ src/view/tabs/Tabs.tsx
@@ -1,12 +1,38 @@
 import React, {Fragment,Component} from "react";
 import {Tab} from "../../../lib/tab";
-
+import {TabPane} from "../../../lib/tab/Tab";
 
 export default class Tabs extends Component{
-
     render(){
         return <Fragment>
-            <Tab />
+            <Tab checkedKey="3">
+                <Tab.Pane key="1" tab="Tab1">
+                    <div style={{padding: "16px 24px"}}>
+                        <div>热烈庆祝中华人名共和国成立70周年!</div>
+                    </div>
+                </Tab.Pane>
+                <TabPane key="2" disabled tab="Tab2">
+                    <div style={{padding: "16px 24px"}}>
+                        <div>热烈庆祝中华人名共和国成立70周年!</div>
+                        <div>热烈庆祝中华人名共和国成立70周年!</div>
+                    </div>
+                </TabPane>
+                <TabPane key="3" tab="Tab3">
+                    <div style={{padding: "16px 24px"}}>
+                        <div>热烈庆祝中华人名共和国成立70周年!</div>
+                        <div>热烈庆祝中华人名共和国成立70周年!</div>
+                        <div>热烈庆祝中华人名共和国成立70周年!</div>
+                    </div>
+                </TabPane>
+                <TabPane key="4" tab="Tab4">
+                    <div style={{padding: "16px 24px"}}>
+                        <div>热烈庆祝中华人名共和国成立70周年!</div>
+                        <div>热烈庆祝中华人名共和国成立70周年!</div>
+                        <div>热烈庆祝中华人名共和国成立70周年!</div>
+                        <div>热烈庆祝中华人名共和国成立70周年!</div>
+                    </div>
+                </TabPane>
+            </Tab>
         </Fragment>
     }
 }
